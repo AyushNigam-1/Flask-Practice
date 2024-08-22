c55932fd0fbc9aeb6c989e85d4ca0ed66f70066b
Ayush <ayushnigam843@gmail.com>
2024-08-22 21:21:51 +0530

    Added Flask Routes & Templates

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask , render_template , request
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.methods=='POST':
+        name = request.method['name']
+        return f'Hello {name}'
+    return render_template('form.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    res = ""
+    if score>=50:
+        res = "PASS"
+    else:
+        res = "FAIL"
+
+    return render_template("index.html",results=res)
+
+if __name__=='__main__':
+    app.run(debug=True)
