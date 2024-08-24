081eb23fd18503c98f0ab660f585b9ee508e9a0e
Ayush <ayushnigam843@gmail.com>
2024-08-24 20:51:35 +0530

    Added Session , Flash Messages & File Handling

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask , render_template , request , redirect , url_for
+from flask import Flask , render_template , request , redirect , url_for , abort , flash , session
 
 app = Flask(__name__)
-
+app.secret_key = "abc"
 @app.route("/")
 def welcome():
     return render_template("index.html")
 
 @app.route('/form',methods=['GET','POST'])
 def form():
-    if request.methods=='POST':
-        sc = request.form['score']
-        total = request.form['total']
-        return redirect(url_for(f'/success',score=sc+total))
+    if request.method=='POST':
+        sc = int(request.form['score'])
+        total = int(request.form['total'])
+        f = request.files['file']
+        f.save(f.filename)
+        session['key'] = sc
+        flash("User Data Saved !")
+        return redirect(url_for(f'success',score=sc+total))
     return render_template('form.html')
 
 @app.route('/success/<int:score>')
