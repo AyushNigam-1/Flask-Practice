754ec99217c3e171fcfbc96f6d4f35f671c275c7
Ayush <ayushnigam843@gmail.com>
2024-08-26 20:59:06 +0530

    Created SQL Connection

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask , render_template , request , redirect , url_for , abort , flash , session , make_response
-
+from flask.mysqldb import MySQL
 app = Flask(__name__)
 app.secret_key = "abc"
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "mydb"
+
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    fname = "Ayush"
+    lname = "Nigam"
+    curr = mysql.connection_cursor()
+    curr.execute("INSERT INTO user(fname , lname) VALUES(%s, %s)" , {fname,lname})
+    mysql.connection.connect()
+    curr.close()
+    return "SUccess"
+
 @app.route("/")
 def welcome():
     return render_template("index.html")
