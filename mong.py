da2966cbf79abc4339b85195ed19de91f6b30ed8
Ayush <ayushnigam843@gmail.com>
2024-09-03 20:49:52 +0530

    Added User Validation Route

--- mong.py
+++ mong.py
@@ -2,7 +2,8 @@ from flask import Flask , abort , render_template , request , session , redirect
 from flask_pymongo import PyMongo
 from mongoconfig import config
 import json
-import datetime
+import time
+import re
 from hashlib import sha256
 app = Flask(__name__)
 app.secret_key  = b'kjsdfjbdf/sjdnf'
@@ -15,9 +16,29 @@ def show_login():
     if 'signupSuccess' in session:
         signupSuccess = session['signupSuccess']
         session.pop('signupSuccess',None)
+
+    error = ''
+    if 'error' in session:
+        error = session['error']
+        session.pop('error',None)
     return render_template('login.html',signupSuccess=signupSuccess)
 
 
+def check_login():
+    try:
+        email = request.form['username']
+    except KeyError:
+        email = ''
+    try:
+        password = request.form['password']
+    except KeyError:
+        password = ''
+    
+    if not len(email)>0:
+        session['error'] = 'Email is required'
+        return redirect('/login')
+
+    user_documents = mongo.db.users.find({"email":email})
 # @app.route("/")
 # def hello_world():
 #     teach = mongo.db.teachers.find()
@@ -39,42 +60,46 @@ def show_signup():
     return render_template('signup.html',error=error)
 
 @app.route("/" , methods=['GET','POST'])
-def login ():
-    if request.method == 'POST':
-        # try:
-        #    email = request.form['username']
-        # except KeyError:
-        #    email = ''
-        # try:
-        #    password = request.form['password']
-        # except KeyError:
-        #    password = ''
+def signup ():
+    print(request.form['email'] ,request.form['password'] )
+    # if request.method == 'POST':
+    #     try:
+    #        email = request.form['username']
+    #     except KeyError:
+    #        email = ''
+    #     try:
+    #        password = request.form['password']
+    #     except KeyError:
+    #        password = ''
 
-        # if not len(email) > 0 or not '@' in email or not '.' in email:
-        #     session['error'] = 'Email is required'
-        #     return redirect('/')
+    #     if validate_email(email):
+    #         session['error'] = 'Email is required'
+    #         return redirect('/')
         
-        # if not len(password) > 0:
-        #     session['error'] = 'Password is required'
-        #     return redirect('/')
+    #     if not len(password) > 0:
+    #         session['error'] = 'Password is required'
+    #         return redirect('/')
         
-        # user_count = mongo.db.users.count_documents({"email" :email})
-        # if user_count > 0:
-        #     session['error']= 'Email already exists'
-        #     return redirect('/')
+    #     user_count = mongo.db.users.count_documents({"email" :email})
+    #     if user_count > 0:
+    #         session['error']= 'Email already exists'
+    #         return redirect('/')
           
-        result = mongo.db.users.insert_one({
-            'email':request.form['username'],
-            'password':sha256(request.form['password'].encode('utf-8')).hexdigest(),
-            'name':'',
-            'lastLoginDate':None,
-            # 'createdAt':datetime.utcnow,
-            'updatedAt':123
-        })
-        session['signupSuccess'] = 'Your user account is ready . You can log'
+    #     result = mongo.db.users.insert_one({
+    #         'email':request.form['username'],
+    #         'password':sha256(request.form['password'].encode('utf-8')).hexdigest(),
+    #         'name':'',
+    #         'lastLoginDate':None,
+    #         'createdAt':time.time(),
+    #         'updatedAt':time.time()
+    #     })
+    #     session['signupSuccess'] = 'Your user account is ready . You can log'
         # return f"{email} , {password}"
-    return render_template('login.html')
+    return render_template('signup.html')
 
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
 
 if __name__ == '__main__':
     app.run(debug= True)
